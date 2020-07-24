c5429e9084d79b8ef760072ee115e7a6f81c079f
Dmitrii Ershov <dmitryershov0@gmail.com>
2020-07-24 06:24:39 +0300

    Initial project

---
+++ Main/CurrentConditions.cs
@@ -0,0 +1,68 @@
+﻿using Accuweather.Core.Helpers;
+using Accuweather.Core;
+using System.Threading.Tasks;
+
+namespace Accuweather.Current
+{
+	/// <summary>
+	///	Accuweather Current Conditions Api
+	/// </summary>
+	public class CurrentConditions : AccuweatherApiCore, ICurrentConditions
+	{
+		private const string _baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="apiKey">Api key accuweather.</param>
+		/// <param name="language">Language.</param>
+		/// <returns></returns>
+		public CurrentConditions(string apiKey, string language = "en-us") : base(apiKey, language)
+		{
+
+		}
+		
+		public async Task<string> Get(int locationKey, bool details = false)
+		{
+			var obj = new
+			{
+				language = _language,
+				details
+			};
+			var url = $"{_baseUrl}{locationKey}?apikey={_apiKey}&";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		public async Task<string> GetTopCities(int group)
+		{
+			var obj = new
+			{
+				language = _language
+			};
+			var url = $"{_baseUrl}topcities/{group}?apikey={_apiKey}&";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		public async Task<string> GetHistorical24Hours(int locationKey, bool details = false)
+		{
+			var obj = new
+			{
+				language = _language,
+				details
+			};
+			var url = $"{_baseUrl}{locationKey}/historical/24?apikey={_apiKey}&";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		public async Task<string> GetHistorical6Hours(int locationKey, bool details = false)
+		{
+			var obj = new
+			{
+				language = _language,
+				details
+			};
+			var url = $"{_baseUrl}{locationKey}/historical?apikey={_apiKey}&";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+	}
+}
---
+++ Main/ICurrentConditions.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+
+namespace Accuweather.Current
+{
+	/// <summary>
+	/// Get Current Conditions data for a specific location.
+	/// </summary>
+	public interface ICurrentConditions
+	{
+		/// <summary>
+		/// Returns current conditions data for a specific location. Current conditions searches require a location key. Please use the Locations API to obtain the location key for your desired location.
+		///  By default, a truncated version of the current conditions data is returned. The full object can be obtained by passing "details=true" into the url string.
+		/// </summary>
+		/// <param name="locationKey">Unique ID to search for a specific location.</param>
+		/// <param name="details">Boolean value specifies whether or not to include full details in the response.</param>
+		/// <returns></returns>
+		Task<string> Get(int locationKey, bool details = false);
+
+		/// <summary>
+		/// Returns current conditions data for the top 50, 100, or 150 cities worldwide, based on rank.
+		/// </summary>
+		/// <param name="group">Integer that indicates the number to return with the request. Current supported values are 50, 100 and 150.</param>
+		/// <returns></returns>
+		Task<string> GetTopCities(int group);
+
+		/// <summary>
+		/// Returns historical current conditions data for a specific location. Current conditions searches require a location key. Please use the Locations API to obtain the location key for your desired location. 
+		/// </summary>
+		/// <param name="locationKey">Unique ID to search for a specific location.</param>
+		/// <param name="details">Boolean value specifies whether or not to include full details in the response.</param>
+		/// <returns></returns>
+		Task<string> GetHistorical24Hours(int locationKey, bool details = false);
+
+		/// <summary>
+		/// Returns historical current conditions data for a specific location. Current conditions searches require a location key. Please use the Locations API to obtain the location key for your desired location. 
+		/// </summary>
+		/// <param name="locationKey">Unique ID to search for a specific location.</param>
+		/// <param name="details">Boolean value specifies whether or not to include full details in the response.</param>
+		/// <returns></returns>
+		Task<string> GetHistorical6Hours(int locationKey, bool details = false);
+	}
+}
